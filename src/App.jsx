85aadb9dfc7e6ad35b0fb749a1c8bf59e2e1fa8b
Monowar Hussain <monowargtp@gmail.com>
2023-09-13 16:21:37 +0530

    Add  "About me" and social accounts

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Monowar Hussain",
+  title: "Founder of Enbiit | Professional Trainer",
+  email: "monowar@enbiit.com",
+  gitHub: "techmonowar",
+  instagram: "techmonowar",
+  linkedIn: "techmonowar",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "techmonowar",
+  youTube: "monowar",
 };
 
 const primaryColor = "#4E567E";
